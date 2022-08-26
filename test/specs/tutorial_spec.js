651c0259cde1ac01f2bff5df19ab4217ebf673b4
Prajwal Dhatwalia <dh.prajwal@gmail.com>
2022-08-26 00:26:29 -0400

    Adds inputs to webpage

--- test/specs/tutorial_spec.js
+++ test/specs/tutorial_spec.js
@@ -4,5 +4,14 @@ describe('The tutorial', () => {
 
         $('#name').waitForEnabled(3000);
         $('#comments').waitForEnabled(3000);
+
+        $('#name').setValue('Prajwal D');
+        $('#state').selectByVisibleText('Texas');
+
+        var backpack = $('#backpack');
+        browser.execute('arguments[0].scrollIntoView();', backpack);
+        backpack.click();
+
+        $('#newsletter_yes').click();
     });
 });
